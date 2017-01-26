a625d936df7ca00baa73a5a56ca78e714e02335c
K8718 <K8718@student.jamk.fi>
2017-01-26 15:33:33 +0200

    Aloitus

---
+++ Teht03/TV.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht03
+{
+    class TV
+    {
+        // http://student.labranet.jamk.fi/~mapas/k2017/TTOS0200/index.php?section=olio&page=exercises
+    }
+}
