88fe292e371d937022ae35da784eeab98f7be38d
K8718 Malmberg Jimi <k8718@student.jamk.fi>
2017-02-02 11:18:38 +0200

    Runko valmis

---
+++ Teht05/Opiskelija.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht05
+{
+    class Opiskelija
+    {
+        public string Nimi { get; set; }
+        public string Tunnus { get; set; }
+        public int Ojaksot { get; set; }
+        public int Opisteet { get; set; }
+
+        public void Palauta()
+        {
+            Opisteet += 3;
+            Console.WriteLine("Hienoa palautit rästitehtävät, nyt sinulla on opintopisteitä " + Opisteet);
+        }
+
+        public void Osallistu()
+        {
+            Ojaksot += 1;
+            Console.WriteLine("Osallistuit uudelle opintojaksolle. Nyt olet " + Ojaksot + " opintojaksolla.");
+        }
+
+        public void EiOsallistu()
+        {
+            Ojaksot -= 1;
+            Console.WriteLine("Jätit yhden opintojakson pois. Sinulla on enää " + Ojaksot + " opintojaksoa.");
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine(Nimi);
+            Console.WriteLine(Tunnus);
+            Console.WriteLine("Opintojaksoja: " + Ojaksot);
+            Console.WriteLine("Opintopisteitä: " + Opisteet);
+
+        }
+    }
+}
---
+++ Teht05/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija heikko = new Opiskelija();
+            heikko.Nimi = "Heikko Hyyppä";
+            heikko.Tunnus = "K1234";
+            heikko.Ojaksot = 5;
+            heikko.Opisteet = 27;
+            heikko.PrintData();
+            heikko.EiOsallistu();
+
+            Console.WriteLine(); // ENTER
+
+            Opiskelija tarmo = new Opiskelija();
+            tarmo.Nimi = "Tarmo Tarmokas";
+            tarmo.Tunnus = "K4321";
+            tarmo.Ojaksot = 9;
+            tarmo.Opisteet = 45;
+            tarmo.PrintData();
+            tarmo.Osallistu();
+
+            Console.WriteLine(); // ENTER
+
+            Opiskelija keiju = new Opiskelija();
+            keiju.Nimi = "Keiju Hammas";
+            keiju.Tunnus = "K1010";
+            keiju.Ojaksot = 1;
+            keiju.Opisteet = 10;
+            keiju.PrintData();
+            keiju.Palauta();
+
+            Console.WriteLine(); // ENTER
+        }
+    }
+}
