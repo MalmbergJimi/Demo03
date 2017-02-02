7c63fc1e8855a5ccecb90b24a5bff2c7874ed966
K8718 Malmberg Jimi <k8718@student.jamk.fi>
2017-02-02 10:02:35 +0200

    TV toimii

--- Teht03/Program.cs
+++ Teht03/Program.cs
@@ -10,6 +10,32 @@ namespace Teht03
     {
         static void Main(string[] args)
         {
+            TV teevee = new TV();
+            teevee.IsOn = true;
+            teevee.Channel = 40;
+            teevee.Volume = 15;
+            teevee.Recording = false;
+            teevee.Mute = false;
+            teevee.ScreenMode = "GAMES";
+
+            teevee.TvOn();
+            Console.WriteLine("You are watching channel number " + teevee.Channel);
+            teevee.ChangeChanNext();
+            Console.WriteLine("Volume is " + teevee.Volume);
+            teevee.ChangeChanPrev();
+            Console.WriteLine("So to the TV is on? " + teevee.IsOn);
+            Console.WriteLine("Is is Recording? " + teevee.Recording);
+            teevee.Volume = 10;
+            Console.WriteLine("Volume is now " + teevee.Volume);
+            Console.WriteLine("Your screenmode is " + teevee.ScreenMode);
+
+            string screenmode = "MOVIES";
+            Console.WriteLine("Your screenmode is now " + screenmode);
+            teevee.Volume = 20;
+            Console.WriteLine("Volume is now " + teevee.Volume);
+
+            Console.WriteLine();
+           teevee.TvOff();
         }
     }
 }
--- Teht03/TV.cs
+++ Teht03/TV.cs
@@ -8,6 +8,43 @@ namespace Teht03
 {
     class TV
     {
-        // http://student.labranet.jamk.fi/~mapas/k2017/TTOS0200/index.php?section=olio&page=exercises
+        public bool IsOn { get; set; }
+        public int Channel { get; set; }
+        public int Volume { get; set; }
+        public bool Recording { get; set; }
+        public bool Mute { get; set; }
+        public string ScreenMode { get; set; }
+
+        // Default constructor 
+        public TV()
+        {
+
+        }
+        // Constructor Program
+        public TV(string screenmode)
+        {
+            ScreenMode = screenmode;
+        }
+
+        public void TvOn()
+        {
+            Console.WriteLine("TV is now on!");
+        }
+
+        public void TvOff()
+        {
+            Console.WriteLine("TV is off.");
+        }
+
+        public void ChangeChanNext()
+        {
+            Console.WriteLine("You changed the channel, nice choice!");
+        }
+        public void ChangeChanPrev()
+        {
+            Console.WriteLine("You put the previous channel back, boooooriiiing...");
+        }
+
+        
     }
 }
